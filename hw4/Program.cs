598b602f3280ddf27c69fbd014ded45351788012
Alexander Timirov <yudyymal@gmail.com>
2022-12-02 18:54:19 +0500

    hw4

---
+++ hw4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N, а я выдам все чётные от 1 до N");
+string a = Console.ReadLine();
+int numberA = Convert.ToInt32(a);
+
+for (int i = 1; i <= numberA; i++)
+{
+    if ((i % 2) == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+}
