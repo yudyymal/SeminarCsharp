e429524eb5efb416abaa5eace5d50c81e37e10ef
Alexander Timirov <yudyymal@gmail.com>
2022-12-02 18:00:12 +0500

    Homework1-1

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите число 1");
+string a = Console.ReadLine();
+int numberA = Convert.ToInt32(a);
+Console.WriteLine("Введите число 2");
+string b = Console.ReadLine();
+int numberB = Convert.ToInt32(b);
+
+int max = numberA;
+int min = numberB;
+
+if (max == min)
+{
+    Console.WriteLine("Данные числа равны");
+}
+else if (numberB > max)
+{
+    max = numberB;
+    min = numberA;
+}
+if (max != min)
+{
+    Console.WriteLine("Наибольшее из данных чисел - число " + max + " Наименьшее - " + min);
+}
