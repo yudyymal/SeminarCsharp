e3597af0a066ec8ca60a8b619dc145ac8ce80dd0
Alexander Timirov <yudyymal@gmail.com>
2022-12-02 18:42:13 +0500

    Add

---
+++ hw3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число, а я скажу, чётное или нечётное оно");
+string a = Console.ReadLine();
+int numberA = Convert.ToInt32(a);
+
+if ((numberA % 2) == 0)
+{
+    Console.WriteLine("Данное число чётное");
+}
+else 
+{
+Console.WriteLine("Данное число нечётное");
+}
