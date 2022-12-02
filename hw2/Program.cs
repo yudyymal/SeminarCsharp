749b90204bc8b4e9d1f56447cee01e034cb25122
Alexander Timirov <yudyymal@gmail.com>
2022-12-02 18:38:15 +0500

    Add

---
+++ hw2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число 1");
+string a = Console.ReadLine();
+int numberA = Convert.ToInt32(a);
+Console.WriteLine("Введите число 2");
+string b = Console.ReadLine();
+int numberB = Convert.ToInt32(b);
+Console.WriteLine("Введите число 3");
+string с = Console.ReadLine();
+int numberС = Convert.ToInt32(с);
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberС > max)
+{
+    max = numberС;
+}
+Console.WriteLine("Наибольшее из данных чисел - число " + max);
