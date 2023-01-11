9d311ac87b1a769f232b499f417c1a6803c90527
Alexander Timirov <yudyymal@gmail.com>
2023-01-11 14:32:18 +0600

    Добавлен цикл с условием записи в новый массив элемента из старого массива. Так же добавлен метод вывода массива

--- HomeworkIT/Program.cs
+++ HomeworkIT/Program.cs
@@ -1,24 +1,45 @@
 ﻿string[] words = new string[] { "Hello", "2", "World", ":-)" };
-// string[] words = new string[] { "1234", "1567", "-2", "computer science" };
-// string[] words = new string[] { "Russia", "Denmark", "Kazan"};
+
+// Введение метода для определение длины элемента в массиве
 
 int LengthOfWord(string a)
 {
     return a.Length;
 }
 
-    int count = 0;
-    for (int i = 0; i < words.Length; i++)
-    {
-        if (LengthOfWord(words[i]) <= 3)        { count++; }
-    }
+// Счётчик, вычсчитывающий количество элементов в массиве с длиной меньше или равной 3-ём
 
-    System.Console.WriteLine(count);
+int count = 0;
+for (int i = 0; i < words.Length; i++)
+{
+    if (LengthOfWord(words[i]) <= 3)        { count++; }
+}
 
+// Создание нового массива для дальнейшего хранения элементов по условию задачи
 string[] wordsLessFour = new string[count];
 
-// for (int n = 0; n < words.Length; n++)
-// {
-//     Console.WriteLine(LengthOfWord(words[n]));
-//     Console.WriteLine(words[n]);
-// }
+count = 0;
+for (int i = 0; i < words.Length; i++)
+{
+    if (LengthOfWord(words[i]) <= 3)
+    {
+        wordsLessFour[count] = words[i];
+        count++;
+    }
+}
+
+// Метод вывода массива
+
+void PrintArray(string[] array)
+{
+    int lengthOfArray = array.Length;
+    for (int j = 0; j < lengthOfArray; j++)
+    {
+        Console.Write($"{array[j]}   ");
+
+    }
+}
+
+// Выводим вновь созданный массив, исходя из условий задачи
+
+PrintArray(wordsLessFour);
