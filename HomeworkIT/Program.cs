b9c50690c98dcbc40a2b89182431688a381668c6
Alexander Timirov <yudyymal@gmail.com>
2023-01-11 14:10:52 +0600

    Добавлен счётчик, позволяющий узнать количество элементов массива с длиной меньше или равной 3. После чего создаётся массив с данным размером.

--- HomeworkIT/Program.cs
+++ HomeworkIT/Program.cs
@@ -1,12 +1,24 @@
 ﻿string[] words = new string[] { "Hello", "2", "World", ":-)" };
+// string[] words = new string[] { "1234", "1567", "-2", "computer science" };
+// string[] words = new string[] { "Russia", "Denmark", "Kazan"};
 
-int LengthOfWord (string a)
+int LengthOfWord(string a)
 {
     return a.Length;
 }
 
-for (int n = 0; n < words.Length; n++)
-{
-    Console.WriteLine(LengthOfWord(words[n]));
-    Console.WriteLine(words[n]);
-}
+    int count = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (LengthOfWord(words[i]) <= 3)        { count++; }
+    }
+
+    System.Console.WriteLine(count);
+
+string[] wordsLessFour = new string[count];
+
+// for (int n = 0; n < words.Length; n++)
+// {
+//     Console.WriteLine(LengthOfWord(words[n]));
+//     Console.WriteLine(words[n]);
+// }
